62438b0fce92efeb5c8056502771c8dd3759b0be
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-19 18:01:25 -0500

    Created the basic foundation for the dating website using bootstrap container

--- index.php
+++ index.php
@@ -19,46 +19,8 @@ $f3->set('DEBUG', 3);
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>My Pets!</h1>';
-
-    echo "<a href='order'>Order a Pet</a>";
-    //$view = new View;
-    //echo $view->render('views/home.html');
-});
-
-$f3->route('GET /@pet', function($f3, $params) {
-    print_r($params);
-
-    $pet = $params['pet'];
-    echo "You like $pet";
-
-    if($pet =="dog") {
-        echo "<p>Woof!</p>";
-    }
-    else if($pet == 'cat') {
-        echo "<p>Meow!</p>";
-    }
-    else if($pet == 'chicken') {
-        echo "<p>Bok!</p>";
-    }
-    else if($pet == 'cow') {
-        echo "<p>Moo!</p>";
-    }
-    else if($pet == 'horse') {
-        echo "<p>Neigh!</p>";
-    }
-    else {
-        $f3->reroute("");
-
-        //display a 404 error
-        $f3->error(404);
-    }
-});
-
-//Define a lunch route
-$f3->route('GET /order', function() {
-    $view = new View();
-    echo $view->render('views/form1.html');
+    $view = new View;
+    echo $view->render('views/home.html');
 });
 
 //Run fat free
